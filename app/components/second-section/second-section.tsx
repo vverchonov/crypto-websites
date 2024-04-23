df7aa2723d62282a7696b672d8d2a538d03f7e57
Iurii Kondrakov <deezzir@gmail.com>
2024-04-23 02:29:09 -0400

    Add footer and second section

---
+++ app/components/second-section/second-section.tsx
@@ -0,0 +1,7 @@
+export const SecondSection = (props: any) => {
+    return (
+        <div className="flex flex-col min-h-screen justify-center items-center p-2">
+            <h2 className="text-4xl font-bold">Second Section</h2>
+        </div>
+    );
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,30 @@ export default function RootLayout({
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.ico" sizes="any" />
+                <meta property="og:type" content="website" />
+                <meta property="og:site_name" content="Da Puss" />
+                <meta property="og:url" content="https://dapuss.com/" />
+                <meta property="og:title" content="Solana Token" />
+                <meta
+                    property="og:description"
+                    content="The best cat site on the internet!"
+                />
+                <meta
+                    property="og:image"
+                    content="https://dapuss.com/header.png"
+                />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@DAPUSS" />
+                <meta name="twitter:url" content="https://dapuss.com/" />
+                <meta name="twitter:title" content="Solana Token" />
+                <meta
+                    name="twitter:description"
+                    content="The best cat site on the internet!"
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://dapuss.com/header.png"
+                />
             </head>
             <body className={inter.className}>{children}</body>
         </html>
