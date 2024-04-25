1817fd36c897b59ad765928384273dd7f4fadfea
Viacheslav Verchonov <vverchonov@gmail.com>
2024-04-25 14:20:31 -0400

    qwe

--- app/components/sections/sixth-section/imgs-block.tsx
+++ app/components/sections/sixth-section/imgs-block.tsx
@@ -6,39 +6,39 @@ export const ImgsBlock = () => {
           <div className="flex flex-col justify-center lg:flex-row w-full h-1/3">
             <div className="flex justify-center md:w-2/4 lg:justify-start">
               <img
-                className=" w-11/12 lg:w-9/12 h-full object-contain  mirror-y select-none"
+                className="img-shake w-11/12 lg:w-9/12 h-full object-contain  mirror-y select-none"
                 src="./block6/frame1.webp"
               />
             </div>
             <div className="flex justify-center">
               <img
-                className=" w-9/12 lg:w-7/12 h-full select-none"
+                className="img-shake w-9/12 lg:w-7/12 h-full select-none"
                 src="./block6/frame2.webp"
               />
             </div>
           </div>
           <div className="flex flex-col w-full items-center h-1/3">
             <img
-              className=" w-10/12 lg:w-4/12 h-full object-contain select-none"
+              className="img-shake w-10/12 lg:w-4/12 h-full object-contain select-none"
               src="./block6/frame3.webp"
             />
           </div>
           <div className="flex flex-col justify-center lg:flex-row w-full h-1/3">
             <div className="flex justify-center">
               <img
-                className="w-full h-full object-contain mirror-y select-none"
+                className="img-shake w-full h-full object-contain mirror-y select-none"
                 src="./block6/frame4.webp"
               />
             </div>
             <div className="img6-holder flex justify-center lg:justify-start">
               <img
-                className="ms-auto w-10/12 h-full select-none"
+                className="img-shake ms-auto w-10/12 h-full select-none"
                 src="./block6/frame5.webp"
               />
             </div>
             <div className="img6-holder flex justify-center lg:justify-start">
               <img
-                className="ms-auto w-9/12 object-contain h-full select-none"
+                className="img-shake ms-auto w-9/12 mirror-y object-contain h-full select-none"
                 src="./block6/frame6.webp"
               />
             </div>
