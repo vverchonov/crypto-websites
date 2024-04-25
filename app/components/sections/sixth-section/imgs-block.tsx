ae2326fa6e198ac91f9ef09936301da09b08a0e6
Viacheslav Verchonov <vverchonov@gmail.com>
2024-04-25 13:36:03 -0400

    imgs changed

--- app/components/sections/sixth-section/imgs-block.tsx
+++ app/components/sections/sixth-section/imgs-block.tsx
@@ -2,44 +2,44 @@ export const ImgsBlock = () => {
   return (
     <>
       <div className="min-h-screen w-full pt-28 p-4 lg:px-24 pb-28 max-w-screen-2xl content-center">
-        <div className="flex w-full gap-24 items-center flex-col">
-          <div className="flex flex-col justify-center lg:flex-row w-full gap-16">
-            <div className="flex justify-center lg:justify-start">
+        <div className="flex w-full items-center flex-col min-h-screen">
+          <div className="flex flex-col justify-center lg:flex-row w-full h-1/3">
+            <div className="flex justify-center md:w-2/4 lg:justify-start">
               <img
-                className="rotate-[-15deg] w-9/12 select-none"
-                src="./block6/frame.webp"
+                className=" w-11/12 md:w-9/12 h-full object-contain  mirror-y select-none"
+                src="./block6/frame1.webp"
               />
             </div>
             <div className="flex justify-center">
               <img
-                className="rotate-[12deg]  w-10/12 select-none"
-                src="./block6/frame.webp"
+                className=" w-9/12 md:w-7/12 h-full select-none"
+                src="./block6/frame2.webp"
               />
             </div>
           </div>
-          <div className="flex flex-col w-full items-center">
+          <div className="flex flex-col w-full items-center h-1/3">
             <img
-              className="rotate-[5deg] w-9/12 lg:w-4/12 select-none"
-              src="./block6/frame.webp"
+              className=" w-10/12 md:w-4/12 h-full object-contain select-none"
+              src="./block6/frame3.webp"
             />
           </div>
-          <div className="flex flex-col justify-center gap-16 lg:flex-row w-full">
-            <div className="img5-holder flex justify-center">
-              <img
-                className="rotate-[-12deg] w-10/12 lg:w-9/12 select-none"
-                src="./block6/frame.webp"
-              />
-            </div>
+          <div className="flex flex-col justify-center lg:flex-row w-full h-1/3">
             <div className="flex justify-center">
               <img
-                className="rotate-[-8deg] w-9/12 lg:w-11/12 select-none"
-                src="./block6/frame.webp"
+                className="w-full h-full object-contain mirror-y select-none"
+                src="./block6/frame4.webp"
               />
             </div>
             <div className="img6-holder flex justify-center lg:justify-start">
               <img
-                className="rotate-[5deg] w-10/12 select-none"
-                src="./block6/frame.webp"
+                className="ms-auto w-10/12 h-full select-none"
+                src="./block6/frame5.webp"
+              />
+            </div>
+            <div className="img6-holder flex justify-center lg:justify-start">
+              <img
+                className="ms-auto w-9/12 mirror-y object-contain h-full select-none"
+                src="./block6/frame6.webp"
               />
             </div>
           </div>
