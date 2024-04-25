cf51d8f82755c445426b83d4b59713b3be085728
Viacheslav Verchonov <vverchonov@gmail.com>
2024-04-25 19:12:40 -0400

    lniks

--- app/components/sections/main-section/links.tsx
+++ app/components/sections/main-section/links.tsx
@@ -29,12 +29,12 @@ export const Links = () => {
         <TgIcon iconClass={iconClass} />
       </a>
 
-      {/* <a target="_blank" href={RAYDIUM} className={buttonClass}>
+      <a target="_blank" href={RAYDIUM} className={buttonClass}>
         <RaydiumIcon iconClass={iconClass} />
       </a>
       <a target="_blank" href={JUPITER} className={buttonClass}>
         <JupiterIcon iconClass={iconClass} />
-      </a> */}
+      </a>
       <a target="_blank" href={DEXSCREENER} className={buttonClass}>
         <DexscreenerIcon iconClass={iconClass} />
       </a>
--- app/urls.ts
+++ app/urls.ts
@@ -7,8 +7,8 @@ export const TELEGRAM_DEV = "https://t.me/lettodev";
 export const DEXSCREENER =
   "https://dexscreener.com/solana/ANvvhmTshfyVMDNiXX58Lq6XG3xqq9ZbnbYoo6W1exAc";
 export const RAYDIUM =
-  "https://raydium.io/swap/?inputCurrency=4inoptdokTnnH5cU64CP64WQbeV6gdFUVmiokPfwzbsL&outputCurrency=sol&outputSymbol=4inopt&fixed=out";
+  "https://raydium.io/swap/?inputCurrency=CKvHs1gAdyJUw3GoWQmmQ3fRVWxMqiKv841L7hmfFHyy&outputCurrency=sol&outputSymbol=CKvHs1&fixed=out";
 export const JUPITER =
-  "https://jup.ag/swap/4inoptdokTnnH5cU64CP64WQbeV6gdFUVmiokPfwzbsL-SOL";
+  "https://jup.ag/swap/So11111111111111111111111111111111111111112-CKvHs1gAdyJUw3GoWQmmQ3fRVWxMqiKv841L7hmfFHyy?inAmount=";
 export const DEXTOOLS =
   "https://www.dextools.io/app/en/solana/pair-explorer/ANvvhmTshfyVMDNiXX58Lq6XG3xqq9ZbnbYoo6W1exAc?t=1714085912157";
