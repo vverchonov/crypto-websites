8f516d3ad563bcf5f1a221e874eb9a0db51ef715
Viacheslav Verchonov <vverchonov@gmail.com>
2024-04-27 20:20:22 -0400

    test XR 3

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import { MainSection } from "./components/main-section/main-section";
 import { SecondSection } from "./components/second-section/second-section";
 import { FooterSection } from "./components/footer-section";
 import { ARTest } from "./components/main-section/ar-test";
+import ARView from "./components/main-section/ARView";
 
 export default function Home() {
   const audioRef = useRef();
@@ -23,23 +24,36 @@ export default function Home() {
   };
 
   return (
-    <>
-      <ARButton />
-      <Canvas camera={{ position: [0, 2, 0] }}>
-        <XR referenceSpace="local-floor">
-          <color attach="background" args={["#111"]} />
-          <ambientLight intensity={2} />
-          <pointLight position={[20, 10, -10]} intensity={2} />
-          <primitive object={new THREE.AxesHelper(2)} />
-          <primitive object={new THREE.GridHelper(20, 20)} />
-          <OrbitControls />
+    //@ts-ignore
+    <model-viewer
+      alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
+      src="./Horse.glb"
+      ar
+      ar-modes="webxr scene-viewer quick-look"
+      //   environment-image="shared-assets/environments/moon_1k.hdr"
+      poster="poster.webp"
+      seamless-poster
+      shadow-intensity="1"
+      camera-controls
+      //@ts-ignore
+    ></model-viewer>
+    // <>
+    //   <ARButton />
+    //   <Canvas camera={{ position: [0, 2, 0] }}>
+    //     <XR referenceSpace="local-floor">
+    //       <color attach="background" args={["#111"]} />
+    //       <ambientLight intensity={2} />
+    //       <pointLight position={[20, 10, -10]} intensity={2} />
+    //       <primitive object={new THREE.AxesHelper(2)} />
+    //       <primitive object={new THREE.GridHelper(20, 20)} />
+    //       <OrbitControls />
 
-          <Box key="companionCube">
-            <meshPhongMaterial color="#440066" />
-          </Box>
-        </XR>
-      </Canvas>
-    </>
+    //       <Box key="companionCube">
+    //         <meshPhongMaterial color="#440066" />
+    //       </Box>
+    //     </XR>
+    //   </Canvas>
+    // </>
     // <motion.div
     //   initial={{
     //     opacity: 0,
