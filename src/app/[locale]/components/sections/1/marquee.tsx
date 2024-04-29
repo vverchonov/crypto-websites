8b7caff493d4e24fafa32e97ebbad38ef229761c
Viacheslav Verchonov <vverchonov@gmail.com>
2024-04-28 22:41:34 -0400

    block 1 done

---
+++ src/app/[locale]/components/sections/1/marquee.tsx
@@ -0,0 +1,20 @@
+import Marquee from "react-fast-marquee";
+
+export const MarqueeCustom = () => {
+  return (
+    <div className="w-full my-16">
+      <Marquee autoFill>
+        <img className="w-6/12" src="./1/marquee.jpg" />
+        <img className="w-6/12" src="./1/marquee.jpg" />
+        <img className="w-6/12" src="./1/marquee.jpg" />
+        <img className="w-6/12" src="./1/marquee.jpg" />
+        <img className="w-6/12" src="./1/marquee.jpg" />
+        <img className="w-6/12" src="./1/marquee.jpg" />
+        <img className="w-6/12" src="./1/marquee.jpg" />
+        <img className="w-6/12" src="./1/marquee.jpg" />
+        <img className="w-6/12" src="./1/marquee.jpg" />
+        <img className="w-6/12" src="./1/marquee.jpg" />
+      </Marquee>
+    </div>
+  );
+};
--- src/app/[locale]/components/sections/1/section.tsx
+++ src/app/[locale]/components/sections/1/section.tsx
@@ -1,22 +1,35 @@
 import { useTranslations } from "next-intl";
+
 import { Links } from "../../common/links";
 import { Wrapper } from "../../common/wrappers/wrapper";
 import { RegularText } from "../../common/text/regular-text";
+import { MarqueeCustom } from "./marquee";
 
 export const SectionOne = (props: any) => {
   const t = useTranslations("SectionOne");
   return (
     <>
-      <div className="min-h-screen w-full">
+      <div className="min-h-screen w-full relative">
         <Links />
         <Wrapper>
-          <RegularText
-            customClass={"text-center"}
-            text={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            }
-          />
+          <div className="flex flex-col gap-8 items-center">
+            <RegularText customClass={"text-center"} text={t("1")} />
+            <img className="w-[50%]" src={"./1/game.png"} />
+          </div>
         </Wrapper>
+        <div className="w-full absolute bottom-0 left-0">
+          <div className="flex">
+            <img className="ml-16 h-64 w-auto" src="./1/bear-1.png" />
+            <img
+              className="mr-16 h-64 w-auto ms-auto mirror-y"
+              src="./1/bear-2.png"
+            />
+          </div>
+          <img className="w-full" src={"./1/road.png"} />
+        </div>
+      </div>
+      <div className="w-full">
+        <MarqueeCustom />
       </div>
     </>
   );
