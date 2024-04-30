43f732451acbd2436b6b185a87320985fe99d1ff
Viacheslav Verchonov <vverchonov@gmail.com>
2024-04-29 20:50:10 -0400

    banned done

--- src/app/[locale]/components/common/welcome-banner.tsx
+++ src/app/[locale]/components/common/welcome-banner.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-const imgClass = "cursor-pointer w-64 h-auto";
+const imgClass = "cursor-pointer mb-4 h-16 md:mb-0 lg:h-24";
 
 export const WelcomeBanner = (props: any) => {
   const [showBanner, setShowBanner] = useState(true);
@@ -11,18 +11,41 @@ export const WelcomeBanner = (props: any) => {
     setShowBanner(false);
   };
 
+  const onHover = (e: any) => {
+    const noButton = document.querySelector("#noButton");
+    if (!noButton) {
+      return;
+    }
+    //@ts-ignore
+    var x = Math.random() * (window.innerWidth - noButton.offsetWidth);
+    //@ts-ignore
+    var y = Math.random() * (window.innerHeight - noButton.offsetHeight);
+
+    //@ts-ignore
+    noButton.style.position = "absolute";
+    //@ts-ignore
+    noButton.style.left = `${x}px`;
+    //@ts-ignore
+    noButton.style.top = `${y}px`;
+  };
+
   return (
     <>
       {showBanner ? (
-        <div className="sticky top-0 left-0 w-full h-[100vh] z-20 bg-black p-36 justify-center align-center">
-          <div className="div-fw z-50 flex">
-            <div className="flex gap-16 h-fit">
+        <div className="sticky top-0 left-0 w-full h-[100vh] z-20 bg-black justify-center align-center">
+          <div className="div-fw h-fit w-full flex justify-center align-center">
+            <div className="flex md:flex-row flex-col justify-end md:items-center">
               <img
                 onClick={onClick}
-                className={imgClass}
-                src={"./banner/yes.png"}
+                className={imgClass + " "}
+                src="./banner/yes.png"
               />
-              <img className={imgClass} src={"./banner/no.png"} />
+              <img
+                id="noButton"
+                onMouseEnter={onHover}
+                className={imgClass + " transition duration-300 ease-in-out"}
+                src="./banner/no.png"
+              />
             </div>
           </div>
         </div>
