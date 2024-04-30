28c8f73dcc8f8df02651e60f70cff41a2de9f256
Viacheslav Verchonov <vverchonov@gmail.com>
2024-04-30 18:43:17 -0400

    new VR

--- src/app/[locale]/components/sections/section-3/preview.tsx
+++ src/app/[locale]/components/sections/section-3/preview.tsx
@@ -2,23 +2,23 @@ export default function Preview(props: any) {
   return (
     <div className="flex flex-col items-center lg:flex-row gap-16 justify-center">
       <img
-        className="w-full md:w-2/3 lg:w-1/3 select-none rounded-lg "
-        src={"/section-3/example.png"}
+        className="w-full md:w-2/3 lg:w-2/12 select-none rounded-lg "
+        src={"/section-3/vr/VR1.png"}
         alt="image1"
       />
       <img
-        className="w-full md:w-2/3 lg:w-1/3 select-none rounded-lg "
-        src={"/section-3/example.png"}
+        className="w-full md:w-2/3 lg:w-2/12 select-none rounded-lg "
+        src={"/section-3/vr/VR2.png"}
         alt="image2"
       />
       <img
-        className="w-full md:w-2/3 lg:w-1/3 select-none rounded-lg "
-        src={"/section-3/example.png"}
+        className="w-full md:w-2/3 lg:w-2/12 select-none rounded-lg "
+        src={"/section-3/vr/VR3.png"}
         alt="image3"
       />
       <img
-        className="w-full md:w-2/3 lg:w-1/3 select-none rounded-lg "
-        src={"/section-3/example.png"}
+        className="w-full md:w-2/3 lg:w-2/12 select-none rounded-lg "
+        src={"/section-3/vr/VR4.png"}
         alt="image4"
       />
     </div>
