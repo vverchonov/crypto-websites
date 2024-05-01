7b81fed582ce257e93302d950ba9341e93bf8365
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-01 18:16:37 -0400

    test 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
       {/* @ts-ignore */}
       <model-viewer
         ref={modelViewerRef}
-        src="666.glb"
+        src="test3.glb"
         ar
         ar-modes="scene-viewer webxr quick-look"
         camera-controls
