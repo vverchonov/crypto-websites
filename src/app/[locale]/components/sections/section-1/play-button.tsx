0698de95f10c04b19dd24f9ec86be1d0555d44ac
Iurii Kondrakov <deezzir@gmail.com>
2024-05-02 07:00:54 -0400

    Minor touchups

--- src/app/[locale]/components/sections/section-1/play-button.tsx
+++ src/app/[locale]/components/sections/section-1/play-button.tsx
@@ -14,6 +14,8 @@ export const PlayButton = (props: any) => {
           src={game}
           alt="game"
           className="md:w-[40%] mb-32 md:mb-16 z-50 hover:scale-[1.02] transform ease-in-out duration-300 select-none"
+          fetchPriority="high"
+          priority
         />
       </button>
     </>
