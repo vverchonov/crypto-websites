3c79d9795f0560a3bdadcd2a81e1f1637486010f
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-02 11:43:00 -0400

    ca

--- src/app/[locale]/urls.ts
+++ src/app/[locale]/urls.ts
@@ -1,16 +1,19 @@
-import { PublicKey } from '@solana/web3.js';
+import { PublicKey } from "@solana/web3.js";
 
-export const CA = 'Coming';
-export const TELEGRAM = 'https://t.me/smobsolana'
-export const TWITTER = 'https://twitter.com/smobsolana'
-export const BUY_LING = 'coming'
-export const DEXSCREENER = 'coming'
-export const RAYDIUM = 'coming'
-export const JUPITER = 'coming'
-export const DEXTOOLS = 'coming'
+export const CA = "7Cga2epeJMhte3j74ekEBRMBDn2nRKcgFBrEggSC99fk";
+export const TELEGRAM = "https://t.me/smobsolana";
+export const TWITTER = "https://twitter.com/smobsolana";
+export const BUY_LING = "coming";
+export const DEXSCREENER = "coming";
+export const RAYDIUM = "coming";
+export const JUPITER = "coming";
+export const DEXTOOLS = "coming";
 
-export const TELEGRAM_DEV = 'https://t.me/lettodev'
-export const TWITTER_DEV = 'https://twitter.com/letto_dev'
+export const TELEGRAM_DEV = "https://t.me/lettodev";
+export const TWITTER_DEV = "https://twitter.com/letto_dev";
 
-export const RPC_ENDPOINT = 'https://withered-crimson-sun.solana-devnet.quiknode.pro/5df09d3d4a53046d05db415a893610388d405d59/'
-export const CANDY_MACHINE = new PublicKey('Fkg8MFxwcuDPZxAU6sjfJ7AwrrJ9YRS4FMfm4MnKMetR')
+export const RPC_ENDPOINT =
+  "https://withered-crimson-sun.solana-devnet.quiknode.pro/5df09d3d4a53046d05db415a893610388d405d59/";
+export const CANDY_MACHINE = new PublicKey(
+  "Fkg8MFxwcuDPZxAU6sjfJ7AwrrJ9YRS4FMfm4MnKMetR"
+);
