b6f531659512a8faae185d280f185e47d42d9067
Iurii Kondrakov <deezzir@gmail.com>
2024-05-01 21:11:51 -0400

    Fix model issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         />
         <meta
           property="og:image"
-          content="https://ar.smobsolana.com/header.png"
+          content="https://ar.smobsolana.com/header.webp"
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@smobsolana" />
@@ -34,38 +34,14 @@ export default function RootLayout({
         <meta name="twitter:description" content="Smoking Depressed Bear" />
         <meta
           name="twitter:image"
-          content="https://ar.smobsolana.com/header.png"
+          content="https://ar.smobsolana.com/header.webp"
         />
         <script
           type="module"
           src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.4.0/model-viewer.min.js"
         ></script>
-        <link type="text/css" href="./styles.css" rel="stylesheet" />
-        <script src="./script.js"></script>
       </head>
-      <body>
-        {/* @ts-ignore */}
-        <model-viewer
-          src="Astronaut.glb"
-          ar
-          ar-modes="scene-viewer webxr quick-look"
-          camera-controls
-          tone-mapping="neutral"
-          poster="poster.webp"
-          shadow-intensity="1"
-        >
-          <div className="progress-bar hide" slot="progress-bar">
-            <div className="update-bar"></div>
-          </div>
-          <button slot="ar-button" id="ar-button">
-            View in your space
-          </button>
-          <div id="ar-prompt">
-            <img src="ar_hand_prompt.png" />
-          </div>
-          {/* @ts-ignore */}
-        </model-viewer>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,60 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+
 export default function Home() {
-  return <main></main>;
+  const modelViewerRef = useRef<any>(null);
+  const [progress, setProgress] = useState(0);
+
+  const onProgress = (event: { detail: { totalProgress: any } }) => {
+    const totalProgress = event.detail.totalProgress;
+    setProgress(totalProgress * 100);
+    if (totalProgress === 1) {
+      const modelViewer = modelViewerRef.current;
+      if (modelViewer) {
+        modelViewer?.removeEventListener("progress", onProgress);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const modelViewer = modelViewerRef.current;
+    if (modelViewer) {
+      modelViewer.addEventListener("progress", onProgress);
+    }
+
+    return () => {
+      if (modelViewer) {
+        modelViewer.removeEventListener("progress", onProgress);
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      {/* @ts-ignore */}
+      <model-viewer
+        ref={modelViewerRef}
+        src="666.glb"
+        ar
+        ar-modes="scene-viewer webxr quick-look"
+        camera-controls
+        ar-status="not-presenting"
+        tone-mapping="neutral"
+        poster="poster.webp"
+        shadow-intensity="1"
+        autoplay
+      >
+        <div className={`progress-bar ${progress === 100 ? "hide" : ""}`}>
+          <div className="update-bar" style={{ width: `${progress}%` }}></div>
+        </div>
+        <button slot="ar-button" id="ar-button">
+          View in your space
+        </button>
+        <div id="ar-prompt">
+          <img src="ar_hand_prompt.webp" />
+        </div>
+        {/* @ts-ignore */}
+      </model-viewer>
+    </>
+  );
 }
