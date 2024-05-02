cdd5f9f6028f93c6438a16ca4bd861531231a33e
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-02 00:35:53 -0400

    new AR photos

--- src/app/[locale]/components/sections/section-3/preview.tsx
+++ src/app/[locale]/components/sections/section-3/preview.tsx
@@ -3,22 +3,22 @@ export default function Preview(props: any) {
     <div className="flex flex-col items-center lg:flex-row gap-16 justify-center max-w-screen-2xl">
       <img
         className=" w-10/12 lg:w-2/12 select-none rounded-lg transform transition duration-500 hover:scale-110 z-index-50 select-none"
-        src={"/section-3/vr/VR1.png"}
+        src={"/section-3/vr/ar1.webp"}
         alt="image1"
       />
       <img
         className=" w-10/12 lg:w-2/12 select-none rounded-lg transform transition duration-500 hover:scale-110 z-index-50 select-none"
-        src={"/section-3/vr/VR2.png"}
+        src={"/section-3/vr/ar2.webp"}
         alt="image2"
       />
       <img
         className=" w-10/12 lg:w-2/12 select-none rounded-lg transform transition duration-500 hover:scale-110 z-index-50 select-none"
-        src={"/section-3/vr/VR3.png"}
+        src={"/section-3/vr/ar3.webp"}
         alt="image3"
       />
       <img
         className=" w-10/12 lg:w-2/12 select-none rounded-lg transform transition duration-500 hover:scale-110 z-index-50 select-none"
-        src={"/section-3/vr/VR4.png"}
+        src={"/section-3/vr/ar4.webp"}
         alt="image4"
       />
     </div>
