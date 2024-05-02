b993f2e6ad1127187d448e81fc2645da893a8efc
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-02 15:59:55 -0400

    clear

--- app/urls.ts
+++
@@ -1,9 +0,0 @@
-export const CA = 'COMING';
-export const TELEGRAM = 'https://t.me/mihabibi11'
-export const TWITTER = 'https://twitter.com/mihabibi_'
-export const BUY_LING = 'https://raydium.io/swap/?inputCurrency=4inoptdokTnnH5cU64CP64WQbeV6gdFUVmiokPfwzbsL&outputCurrency=sol&outputSymbol=4inopt&fixed=out'
-export const TELEGRAM_DEV = 'https://t.me/lettodev'
-export const DEXSCREENER = 'https://dexscreener.com/solana/ERCD6qtvn4sWhmNBLyPXfoKghjFRM3gxAYKxNzcw9a5x'
-export const RAYDIUM = 'https://raydium.io/swap/?inputCurrency=4inoptdokTnnH5cU64CP64WQbeV6gdFUVmiokPfwzbsL&outputCurrency=sol&outputSymbol=4inopt&fixed=out'
-export const JUPITER = 'https://jup.ag/swap/4inoptdokTnnH5cU64CP64WQbeV6gdFUVmiokPfwzbsL-SOL'
-export const DEXTOOLS = 'https://www.dextools.io/app/en/solana/pair-explorer/ERCD6qtvn4sWhmNBLyPXfoKghjFRM3gxAYKxNzcw9a5x?t=1713314960171'
