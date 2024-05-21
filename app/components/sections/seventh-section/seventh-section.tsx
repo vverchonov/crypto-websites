9389204985be6d6f9436ab57b779ead93d917d40
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-21 17:11:06 -0400

    1

--- app/components/sections/seventh-section/seventh-section.tsx
+++ app/components/sections/seventh-section/seventh-section.tsx
@@ -13,7 +13,7 @@ export const SeventhSection = (props: any) => {
           <FloatingBanana
             bananaClass={"rotate-12"}
             key={2}
-            customClass={"absolute right-12 w-fit top-0 z-0 select-none"}
+            customClass={"absolute right-0 w-fit top-0 z-0 select-none"}
           />
           <FloatingBanana
             key={1}
@@ -22,12 +22,12 @@ export const SeventhSection = (props: any) => {
           <FloatingBanana
             bananaClass={"rotate-90"}
             key={3}
-            customClass={"absolute left-48 w-fit bottom-48 z-0 select-none"}
+            customClass={"absolute left-0 w-fit bottom-48 z-0 select-none"}
           />
           <FloatingBanana
             bananaClass={"rotate-4"}
             key={5}
-            customClass={"absolute right-48 w-fit bottom-[50%] z-0 select-none"}
+            customClass={"absolute right-8 w-fit bottom-[50%] z-0 select-none"}
           />
           <p className=" text-4xl leading-loose lg:text-6xl font-bold text-center text-white lg:leading-loose relative z-50">
             {word("4", props.lang)}
