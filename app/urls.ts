244b197fc61b275a6d3cf841e58641cb6fa5a7df
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-21 16:36:04 -0400

    updated

--- app/urls.ts
+++ app/urls.ts
@@ -1,6 +1,6 @@
 export const CA = "Coming";
-export const TELEGRAM = "";
-export const TWITTER = "";
+export const TELEGRAM = "https://t.me/loliensolana";
+export const TWITTER = "https://x.com/alondealien?s=21";
 export const BUY_LING = "";
 
 export const DEXSCREENER = "";
